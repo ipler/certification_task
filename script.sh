efbceefbccd22ce5206cac54549ad115d53426e7
ipler <babeev.v.s@gmail.com>
2023-11-12 08:58:20 +0300

    update_62

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 IP_PROD=$(grep -A1 '\[PROD\]' ./ansible/hosts | grep -v "\[PROD\]")
 export IP_PROD
 echo $IP_PROD
-ssh-keyscan $IP_PROD >> ~/.ssh/known_hosts
-ssh ubuntu@$IP_PROD -i ./aws___key_pair_rsa_1_.pem "docker pull gotofront/webapp:1.0 && docker run -d -p 80:8080 gotofront/webapp:1.0"
+#ssh-keyscan $IP_PROD >> ~/.ssh/known_hosts
+ssh -o 'StrictHostKeyChecking no' ubuntu@$IP_PROD -i ./aws___key_pair_rsa_1_.pem "docker pull gotofront/webapp:1.0 && docker run -d -p 80:8080 gotofront/webapp:1.0"
