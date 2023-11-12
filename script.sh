4cff81af6d8bd54ff3e430a0cd693fab5c8c180b
ipler <babeev.v.s@gmail.com>
2023-11-12 09:39:32 +0300

    update_64

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ export IP_PROD
 echo $IP_PROD
 chmod 400 ./aws___key_pair_rsa_1_.pem
 #ssh-keyscan $IP_PROD >> ~/.ssh/known_hosts
-ssh -o 'StrictHostKeyChecking no' ubuntu@$IP_PROD -i ./aws___key_pair_rsa_1_.pem "docker pull gotofront/webapp:1.0 && docker run -d -p 80:8080 gotofront/webapp:1.0"
+ssh -o 'StrictHostKeyChecking no' ubuntu@$IP_PROD -i ./aws___key_pair_rsa_1_.pem "sudo docker pull gotofront/webapp:1.0 && sudo docker run -d -p 80:8080 gotofront/webapp:1.0"
