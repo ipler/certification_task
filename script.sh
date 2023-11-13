64e2f5256707a6ab39bb2fda8bb022a969ad3272
ipler <babeev.v.s@gmail.com>
2023-11-13 09:43:52 +0300

    init_2

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 IP_PROD=$(grep -A1 '\[PROD\]' ./ansible/hosts | grep -v "\[PROD\]")
 export IP_PROD
 echo $IP_PROD
-chmod 400 ./aws___key_pair_rsa_1_.pem
-ssh ubuntu@$IP_PROD -i ./aws___key_pair_rsa_1_.pem "sudo docker pull gotofront/webapp:1.0 && sudo docker run -d -p 80:8080 gotofront/webapp:1.0"
-rm ./aws___key_pair_rsa_1_.pem
+chmod 400 ./ansible/aws___key_pair_rsa_1_.pem
+ssh ubuntu@$IP_PROD -i ./ansible/aws___key_pair_rsa_1_.pem "sudo docker pull gotofront/webapp:1.0 && sudo docker run -d -p 80:8080 gotofront/webapp:1.0"
+rm ./ansible/aws___key_pair_rsa_1_.pem
